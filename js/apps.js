34338fb0c3d4a7a238254a04d3ea19bc8b446164
Astroknut <charlesbarrett1521@gmail.com>
2017-07-08 21:29:05 -0600

    creates controlled movements

--- js/apps.js
+++ js/apps.js
@@ -1,59 +1,84 @@
 window.onload = function () { 
 	console.log("DOM is ready!"); 
 	startGame();
-	console.log(myGamePiece1, myGamePiece2);
 };
 
 //Declare variables for players
-var myGamePiece1;
-var myGamePiece2; 
+var player1;
+var player2; 
 
 
 //Render raceTrack and players
 function startGame() {
-    myGameArea.start();
-    myGamePiece1 = new component(50, 50, "green", 10, 50);
-    myGamePiece2 = new component(50, 50, "red", 10, 150);
+    gameArea.start();
+    player1 = new component(200, 100, "green", 10, 100);
+    player2 = new component(200, 100, "red", 10, 300);
 }
 
 
-//Creates raceTrack object
-var myGameArea = {
+//Creates raceTrack object. Sets size properties, places canvas on body
+// set update interval of 20 ms, adds event listeners for movement
+var gameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 900;
+        this.canvas.height = 600;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20); 
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+        	gameArea.keys = (gameArea.keys || []);
+        	gameArea.keys[e.keyCode] = true;
+        });
+        window.addEventListener('keyup', function (e) {
+        	gameArea.keys[e.keyCode] = false;
+        });
     },
 
-    //Clears position trail 
+    //Clears gamePiece position trail 
     clear: function() {
     	this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
     }
 };
 
 
-//Constructor for players
+//Constructor for players gamePiece, sets functions for gamePiece moves
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
+    this.speed = 0;
     this.update = function() {    
-    	ctx = myGameArea.context;
+    	ctx = gameArea.context;
     	ctx.fillStyle = color;
     	ctx.fillRect(this.x, this.y, this.width, this.height);
+	},
+	this.newPos = function() {
+		this.x += this.speed;
 	};
 }
 
+
+//update function for 
 function updateGameArea() {
-	myGameArea.clear();
-	myGamePiece1.x += 1;
-	myGamePiece2.x += 2;
-	myGamePiece1.update();
-	myGamePiece2.update();
+	gameArea.clear();
+	player1.speed =  0;
+	player2.speed =  0;
+	if (gameArea.keys && gameArea.keys[39]) {
+		player1.speed = 2;
+	}
+	if (gameArea.keys && gameArea.keys[81]) {
+		player2.speed = 2;
+	}
+	player1.newPos();
+	player2.newPos();
+	player1.update();
+	player2.update();
+}
+
+function movePiece() {
+	this.speed += 1;
 }
 
 //update position of game pieces and clear past positions
